813a8f6b7c4a594db4fadf5a5accb60695d813cc
MalieJobe <maliejobe@outlook.de>
2024-08-12 17:10:12 +0200

    Start working on dropdown component for gender

---
+++ components/Dropdown.vue
@@ -0,0 +1,17 @@
+<template>
+    <select :name="name" :id="name" v-model="selection" @change="emitChange">
+        <option v-for="opt in options" :value="opt">{{ opt.toUpperCase() }}</option>
+    </select>
+</template>
+
+<script setup>
+const props = defineProps({
+    name: { type: String, required: true },
+    options: { type: Array, required: true },
+});
+
+const selection = ref(props.options[0]);
+
+const emit = defineEmits(['selectChange']);
+
+</script>
