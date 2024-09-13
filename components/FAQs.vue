76794e8c2df8755537656004b638f3936f997494
MalieJobe <maliejobe@outlook.de>
2024-09-13 22:25:10 +0200

    minor fix: move items-center to correct element

--- components/FAQs.vue
+++ components/FAQs.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex flex-col gap-y-2 my-8 items-center">
+    <div class="flex flex-col gap-y-2 my-8">
         <div v-for="faq in props.faqs" :key="faq.teaser" class="text-left w-full">
             <div @click="() => changeState(faq.teaser)" @keyup.space="() => changeState(faq.teaser)" tabindex="0"
                 class="text-lg bg-primary bg-opacity-25 hover:bg-opacity-40 rounded
-                        px-3 py-2 cursor-pointer flex justify-between transition-colors">
+                        px-3 py-2 cursor-pointer flex justify-between items-center transition-colors">
                 <h5
                     class="font-medium">
                     {{ faq.teaser }}
