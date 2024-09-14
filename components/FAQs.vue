c7244ccf08e3deb0df81722f8b4f4235e7381f02
MalieJobe <maliejobe@outlook.de>
2024-09-14 18:01:36 +0200

    Minor semantic improvement

--- components/FAQs.vue
+++ components/FAQs.vue
@@ -4,18 +4,18 @@
             <div @click="() => changeState(faq.teaser)" @keyup.space="() => changeState(faq.teaser)" tabindex="0"
                 class="text-md bg-primary bg-opacity-15 hover:bg-opacity-30 rounded
                         px-3 py-2 cursor-pointer flex justify-between items-center transition-colors">
-                <h5
+                <h3
                     class="font-medium pr-2">
                     {{ faq.teaser }}
-                </h5>
+                </h3>
                 <span :class="{'rotate-180': (expanededOne === faq.teaser)}"
                     class=" duration-300 transition-transform">&#9660;</span>
             </div>
-            <div v-html="faq.reveal" 
+            <p v-html="faq.reveal" 
                 :class="{'max-h-96 py-2': (expanededOne === faq.teaser)}"
                 class="max-h-0 transition-all duration-300 collapsible overflow-hidden text-sm
                 bg-primary bg-opacity-5 px-3 rounded-b border border-t-0 border-gray-200">
-            </div>
+            </p>
 
         </div>
     </section>
